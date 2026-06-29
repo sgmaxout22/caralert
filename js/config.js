af2e05f4aba8a11b9933e588cceb3c3378bef4d0
sgmaxout22 <sgmaxout22@gmail.com>
2026-06-29 12:17:00 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbzsHq6sPk1ZdINZW7t_e_ktD5sW4svCK6VFfygtBP1gjHgaT47vmAYy8BxxhydNjRDDYg/exec";
+"https://script.google.com/macros/s/AKfycbwXVE2qnkBc3aQt5DLtk8X8SJvtXEKRx4kY-k3CoXGcO-hyrWvSCBrPVtRIBY3dYAjG4g/exec";
